7eff318cae70950095e1ffb7a0b99bd9aa6aac1c
LiaJahan <jesmoonlia@gmail.com>
2025-10-08 15:53:35 +0600

    js code added for login button

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 document.getElementById('index-button').addEventListener('click', function(e)
 {
     e.preventDefault();
-    console.log('clicked!');
+    
+    // take value from the users
+    const inputMobile = document.getElementById('number-input').value;
+    const mobileNumber = parseInt(inputMobile);
+    
+
+    const inputPin = document.getElementById('pin-input').value;
+    const pinNumber = parseInt(inputPin);
+    
+    // fix value for validation check
+    
+    const stringNumber = '0731565644';
+    const fixMobileNumber = parseInt(stringNumber);
+    const fixPin = 1234;
+    
+    // login build
+    if(mobileNumber === fixMobileNumber && pinNumber === fixPin )
+    {
+        // window.location.href(./home.html);
+        alert('Good Job')
+    }
+    else
+    {
+        alert('wrong input!!')
+    }
 })
