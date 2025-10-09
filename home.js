559dd582d6ca491f301f8926284c64afb563e9c1
LiaJahan <jesmoonlia@gmail.com>
2025-10-09 14:57:17 +0600

    still fixing

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// const validPinNumber = 1234;
 document.getElementById('home-page-button').addEventListener('click', function(e)
 {
     e.preventDefault();
@@ -5,11 +6,29 @@ document.getElementById('home-page-button').addEventListener('click', function(e
     const selectBank = document.getElementById("select-bank").value;
     const bankAccountNumber = document.getElementById('bank-account-number').value;
     const amountToAdd = parseInt(document.getElementById('amount-to-add').value);
-    const pinNumber = document.getElementById('pin-number').value;
+    const pinNumber = parseInt(document.getElementById('pin-number').value);
 
     let availableBalance = parseInt(document.getElementById('available-balance').innerText);
 
     // console.log(availableBalance);
+
+    if(selectBank === "")
+    {
+        alert('please select a bank');
+        return;
+    }
+
+    if(bankAccountNumber.length < 11)
+    {
+        alert('please provide valid Bank Account Number');
+        return;
+    }
+
+    if(pinNumber !== 1234)
+    {
+        alert('please provide valid Pin Number');
+        return;
+    }
 
     availableBalance = availableBalance + amountToAdd;
 
